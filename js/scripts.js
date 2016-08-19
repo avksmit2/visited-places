f56542de1ad22602e4a0971002f2a8cb4f654b74
Angie Smith <avksmit2@gmail.com>
2016-08-19 08:40:01 -0700

    return a number inputted by user.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
+//Back-end
+
+
+//Front-end
 $(document).ready(function() {
   $("form").submit(function(event) {
+    var inputNumber = $("input#number").val();
 
+    console.log(inputNumber);
+    $("ul#numberList").text(inputNumber);
     event.preventDefault();
   })
 })
